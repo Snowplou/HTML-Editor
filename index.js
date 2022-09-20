2769235d68bb4beadf81fa511a1a6e5221e6ff2e
Snowplou <Snowplou01@gmail.com>
2022-09-20 08:39:06 -0700

    Improved css system
    The css is now put into a style tag instead of formatting the css in an array and looping through the information to apply the styles to tags

--- index.js
+++ index.js
@@ -1,61 +1,17 @@
 function updateCode() {
 
     try {
-        document.getElementById("htmlRunner").innerHTML = document.getElementById("htmlInput").value
-        eval(document.getElementById("jsInput").value)
-        
-        let cssOriginal = document.getElementById("cssInput").value
-        let cssId = cssOriginal.split("#")
-        for (let value in cssId) {
-            cssId[value] = cssId[value].split("{")
-            for (let value0 in cssId[value]) {
-                cssId[value][value0] = cssId[value][value0].split("}")
-            }
-        }
-
-        for (let i = 1; i < cssId.length; i++) {
-            let cssIdStyles = cssId[i][1][0].split("\n")
-            for (let style in cssIdStyles) {
-                cssIdStyles[style] = cssIdStyles[style].split(":")
-            }
-            for (let style in cssIdStyles) {
-                if (cssIdStyles[style].length == 2) {
-                    cssIdStyles[style][1] = cssIdStyles[style][1].split(";")[0]
-                    if(document.getElementById(cssId[i][0][0]) != null) document.getElementById(cssId[i][0][0]).style[cssIdStyles[style][0]] = cssIdStyles[style][1]
-                }
-            }
-        }
-
-        let cssClass = cssOriginal.split(".")
-        for (let value in cssClass) {
-            cssClass[value] = cssClass[value].split("{")
-            for (let value0 in cssClass[value]) {
-                cssClass[value][value0] = cssClass[value][value0].split("}")
-            }
-        }
-
-        for (let i = 1; i < cssClass.length; i++) {
-            let cssClassStyles = cssClass[i][1][0].split("\n")
-            for (let style in cssClassStyles) {
-                cssClassStyles[style] = cssClassStyles[style].split(":")
-            }
-            for (let style in cssClassStyles) {
-                if (cssClassStyles[style].length == 2) {
-                    cssClassStyles[style][1] = cssClassStyles[style][1].split(";")[0]
-                    let classGroup = document.getElementsByClassName(cssClass[i][0][0])
-                    for(let i = 0; i < classGroup.length; i++){
-                        classGroup[i].style[cssClassStyles[style][0]] = cssClassStyles[style][1]
-                    }
-                }
-            }
-        }
+        document.getElementById("YhsNWUYhnUYHME").innerHTML = document.getElementById("uygnstdTYbvngyaAyhenw").value
+        eval(document.getElementById("y8dbgntTbfnvwgyBVFYSHGnau").value)
+    
+        document.getElementById("usn3edgwmaoyiua").innerHTML = document.getElementById("ehhnyudwYUgnwuyHUINaueh").value
 
     } catch (err) {
         let para = document.createElement("p");
         para.innerHTML = err;
 
-        document.getElementById("htmlRunner").innerHTML = "";
-        document.getElementById("htmlRunner").appendChild(para)
+        document.getElementById("YhsNWUYhnUYHME").innerHTML = "";
+        document.getElementById("YhsNWUYhnUYHME").appendChild(para)
 
     }
 
